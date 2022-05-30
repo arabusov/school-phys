c3e3949642e86d71eaa09f776045e10f9ac806bb
Andrei Rabusov <arabusov@X31.localdomain>
2022-05-30 11:44:25 +0200

    Try to implement snooker, failed

---
+++ lectures/colliding/snooker.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# This script is C&P from the matplotlib example ``double_pendulum'' and
+# modified in a way that the top particle has fixed Y coordinate.
+# Update (2022): simplify code for a standard pendulum
+#
+# Requirements
+# ============
+#
+#  - python3
+#  - matplotlib
+#  - numpy
+#  - scipy
+# On most of the modern Linux distros (including Slackware) python3 is
+# a part of base system. To install python libraries either use your package
+# manager, or use ``pip3'' command (namely, `pip3 install matplotlib` and so
+# on). MacOS users should also have python3 (and pip3) installed.
+# 
+# Execution
+# =========
+# Type `python3 sliding_pendulum.py` in your GUI Terminal. A new window with
+# animation should appear on your screen automatically, if everything is
+# fine.
+#
+# Copyright (c) 2021 Andrei Rabusov
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from numpy import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import matplotlib.animation as animation
+from collections import deque
+
+G = 9.8  # acceleration due to gravity, in m/s^2
+L = 1.0
+M1 = 1.0  # mass of the first particle (fixed on the X axis)
+M2 = 1.0  # mass of the second particle, the bottom end of the pendulum
+t_stop = 5  # how many seconds to simulate
+history_len = 5000  # how many trajectory points to display
+phi0 = -np.pi/8.
+
+R=0.1
+
+def collide (p1, p2, beta):
+    c, s = np.cos(beta), np.sin(beta)
+    R=np.array (((c, -s), (s, c)))
+    RT=np.array (((c, s), (-s, c)))
+    p1p = R.dot (p1)
+    p2p = R.dot (p2)
+    p1p[1], p2p [1] = p2p [1], p1p [1]
+    return RT.dot(p1p), RT.dot(p2p)
+
+# create a time array from 0..t_stop sampled at 0.02 second steps
+dt = 0.02
+t = np.arange(0, t_stop, dt)
+
+def derivs(state, t):
+
+    dydx = np.zeros_like(state)
+
+    dist2 = (state[0]-state[4])**2 + (state[1]-state[5])**2
+    dist2n = (state[0]+dt*(state[2]-state[6])-state[4])**2
+    + (state[1]+dt*(state[3]-state[7])-state[5])**2
+    if (dist2n <= 4*R**2) and (dist2 >= 4*R**2):
+        print (dist2n, dist2)
+        beta = np.arctan2(state[1]-state[5], state[0]-state[4])
+        p1, p2 = collide (M1*np.array ([state[2], state[3]]),
+                M2*np.array ([state[6], state[7]]), np.pi/2-beta)
+        state[2] = p1 [0]/M1
+        state[3] = p1 [1]/M1
+        state[6] = p2 [0]/M2
+        state[7] = p2 [1]/M2
+
+    dydx[0] = state[2]
+    dydx[1] = state[3]
+    dydx[4] = state[6]
+    dydx[5] = state[7]
+    # F = ma for the first particle
+
+    return dydx
+
+
+v=0.2
+alpha=30*np.pi/180.
+
+# initial state
+# State is 1D array, even components are coordinates, odd --- velocities
+# Now multiply by two (2D problem)
+state = [-0.4*L, 0, v*np.cos(alpha),v*np.sin(alpha), 0,0, 0,0]
+
+# integrate your ODE using scipy.integrate.
+y = np.zeros ((np.size (t), np.size(state)))
+y[0] = state
+for i, titem in enumerate (t):
+    if i > 0:
+        y[i] = derivs (y [i-1], titem)
+
+# Unpack results
+x1 = y[:, 0]
+y1 = y[:, 1]
+x2 = y[:, 4]
+y2 = y[:, 5]
+
+
+fig = plt.figure(figsize=(12.5, 5))
+# Here you can adjust the size of the window and the limits.
+ax = fig.add_subplot(autoscale_on=False, xlim=(-0.7*L, 0.4*L),
+        ylim=(-0.5*L, 0.5*L))
+# Pay attention, that the aspect ratio is 1
+ax.set_aspect('equal')
+ax.grid()
+
+line1, = ax.plot([], [], '-', lw=1)
+line2, = ax.plot([], [], '-', lw=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+phis = np.arange (0, 2*np.pi+np.pi/9, np.pi/9)
+def circle (x, y, r):
+    return r*np.cos (phis) + x, r*np.sin (phis) + y,
+
+def animate(i):
+    thisx1, thisy1 = circle (x1[i], y1[i], R) 
+    thisx2, thisy2 = circle (x2[i], y2[i], R) 
+    print (x1[i], y1 [i])
+
+    line1.set_data(thisx1, thisy1)
+    line2.set_data(thisx2, thisy2)
+
+    time_text.set_text(time_template % (i*dt))
+    return line1, line2, time_text
+
+
+ani = animation.FuncAnimation(
+    fig, animate, len(y), interval=dt*10000, blit=True)
+plt.show()
